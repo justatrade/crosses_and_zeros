8c472d57c040146284d62b6b16e6c3f9c5c90fab
Eugene Myasnikov <justatrade@mail.ru>
2023-11-21 19:31:56 +0300

    - Check win function

--- main.py
+++ main.py
@@ -6,23 +6,27 @@ FIELD_SIZE = 3
 
 
 def choose_first_move():
+    """
+    Making a random choice of a player, making first move
+    :return:
+    """
     return random.choice(['Human', 'AI'])
 
 
 def make_ai_move(p, m) -> None:
-    '''
+    """
     Create a best move for an ai
     :return: return coordinates of a move
-    '''
+    """
     p.make_move(random.choice(p.get_available_moves()), m.sign['AI'])
     m.move_done()
 
 
 def make_human_move(p, m) -> None:
-    '''
+    """
     Get human input as a coordinates
     :return: return coordinates of a move
-    '''
+    """
     cur_move = (-1, -1)
     while cur_move not in p.get_available_moves():
         cur_move = tuple(map(lambda x: int(x), input().split()))
@@ -33,10 +37,10 @@ def make_human_move(p, m) -> None:
 
 
 def main():
-    '''
+    """
     Main management orchestration function
     :return:
-    '''
+    """
     first = choose_first_move()
     second = 'AI' if first == 'Human' else 'Human'
     m = move.Move(FIELD_SIZE, first, second)
